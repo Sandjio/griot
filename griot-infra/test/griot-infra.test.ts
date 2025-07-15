2aef3f046a343e26fe37922becbac403726aa12d
Sandjio <emmasandjio@gmail.com>
2025-07-15 14:26:50 +0100

    feat: initialize AWS CDK infrastructure for Griot manga generation platform

---
+++ griot-infra/test/griot-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GriotInfra from '../lib/griot-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/griot-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GriotInfra.GriotInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
